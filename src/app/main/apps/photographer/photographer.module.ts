4e7c6a3dec8930b12ac1d23c924ffc8128f7e18f
webwerks <raj.kamadiya@neosofttech.com>
2020-08-08 18:28:19 +0530

    add changes in modules

--- src/app/main/apps/photographer/photographer.module.ts
+++ src/app/main/apps/photographer/photographer.module.ts
@@ -1,5 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { AgmCoreModule } from '@agm/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatCardModule } from '@angular/material/card';
+
+
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseWidgetModule } from '@fuse/components/widget/widget.module';
 
 import { PhotographerRoutingModule } from './photographer-routing.module';
 import { AddEditPhotographerComponent } from './add-edit-photographer/add-edit-photographer.component';
@@ -13,8 +35,32 @@ import { PhotographerListService } from './photographer-list.service';
   imports: [
     CommonModule,
     PhotographerRoutingModule,
-    MaterialModule
+    MatButtonModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatTabsModule,
+    MatCardModule,
+    MatProgressBarModule,
+    NgxChartsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD81ecsCj4yYpcXSLFcYU97PvRsE_X8Bx8'
+    }),
+
+    FuseSharedModule,
+    FuseWidgetModule
   ],
-  providers:[PhotographerListService]
+  providers: [PhotographerListService],
+  exports: [
+    AddEditPhotographerComponent, ListPhotographerComponent
+  ]
 })
 export class PhotographerModule { }
